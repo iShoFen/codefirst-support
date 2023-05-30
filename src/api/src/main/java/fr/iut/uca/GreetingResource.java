1ee3d69fe706284bb3b5d76fd19a21be6df46593
Florent MARQUES <florent.marques@etu.uca.fr>
2023-05-30 19:11:06 +0200

    ➕ change mongodb dependency

--- src/api/src/main/java/fr/iut/uca/GreetingResource.java
+++ src/api/src/main/java/fr/iut/uca/GreetingResource.java
@@ -8,9 +8,13 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 import org.bson.Document;
 
+import java.util.ArrayList;
+
 import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Projections.include;
 
 @Path("/api")
 public class GreetingResource {
@@ -23,27 +27,46 @@ public class GreetingResource {
     }
 
     @GET
-    @Path("/feedbacks")
+    @Path("/feedback")
     @Produces(MediaType.APPLICATION_JSON)
-    public String getFeedbacks() {
-        String uri = "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000";
+    public String getFeedback() {
+        String uri = "mongodb://localhost:27017/?directConnection=true&serverSelectionTimeoutMS=2000";
         try (MongoClient mongoClient = MongoClients.create(uri)) {
             MongoDatabase database = mongoClient.getDatabase("codefirst-support");
-            MongoCollection<Document> collection = database.getCollection("feedback");
+            MongoCollection<Document> collection = database.getCollection("feedbacks");
             Document doc = collection.find(eq("author", "samuel")).first();
 
             if (doc != null) {
                 System.out.println(doc.toJson());
-                var createdAt = doc.get("createdAt");
                 return doc.toJson();
             } else {
                 System.out.println("No matching documents found.");
             }
         }
         catch (Exception e) {
+            return e.getMessage();
         }
 
-        return null;
+        return "no result";
+    }
+
+    @GET
+    @Path("/feedbacks")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getFeedbacks() {
+        String uri = "mongodb://localhost:27017/?directConnection=true&serverSelectionTimeoutMS=2000";
+        try (MongoClient mongoClient = MongoClients.create(uri)) {
+            MongoDatabase database = mongoClient.getDatabase("codefirst-support");
+            MongoCollection<Document> collection = database.getCollection("feedbacks");
+
+            var documents = collection.find().projection(include("_id", "author", "content", "created_at")).into(new ArrayList<>());
+            System.out.println(documents);
+            return Response.ok(documents).build();
+        }
+        catch (Exception e) {
+            System.err.println(e.getMessage());
+            return Response.serverError().build();
+        }
     }
 
 }
--- src/api/src/test/java/fr/iut/uca/GreetingResourceTest.java
+++ src/api/src/test/java/fr/iut/uca/GreetingResourceTest.java
@@ -12,10 +12,17 @@ public class GreetingResourceTest {
     @Test
     public void testHelloEndpoint() {
         given()
-          .when().get("/hello")
-          .then()
-             .statusCode(200)
-             .body(is("Hello from RESTEasy Reactive"));
+                .when().get("/api/hello")
+                .then()
+                .statusCode(200);
     }
+
+//    @Test
+//    public void testFeedbacksEndpoint() {
+//        given()
+//                .when().get("/api/feedbacks")
+//                .then()
+//                .statusCode(200);
+//    }
 
 }
