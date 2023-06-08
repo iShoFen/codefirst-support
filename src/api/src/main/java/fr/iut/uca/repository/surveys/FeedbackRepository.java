8910e4e2c667dc3d31427cb35964344bdc54a824
Samuel SIRVEN <samuel.sirven@etu.uca.fr>
2023-06-08 03:06:32 +0200

    👔 update feedback and survey repositories
    - Add some method for get the Feedback and Survey by their date (exact / before / after / between)

--- src/api/src/main/java/fr/iut/uca/repository/surveys/FeedbackRepository.java
+++ src/api/src/main/java/fr/iut/uca/repository/surveys/FeedbackRepository.java
@@ -7,8 +7,6 @@ import com.mongodb.client.result.UpdateResult;
 import com.mongodb.lang.Nullable;
 import fr.iut.uca.DatabaseClient;
 import fr.iut.uca.entity.surveys.FeedbackEntity;
-import fr.iut.uca.entity.surveys.QuestionEntity;
-import fr.iut.uca.entity.surveys.QuestionTypeEntity;
 import fr.iut.uca.model.surveys.Feedback;
 import fr.iut.uca.utils.surveys.FeedbackExtensions;
 import jakarta.enterprise.context.ApplicationScoped;
@@ -16,8 +14,13 @@ import jakarta.inject.Inject;
 import org.bson.Document;
 import org.bson.types.ObjectId;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+
+import static fr.iut.uca.utils.surveys.FeedbackExtensions.CREATED_AT;
+import static fr.iut.uca.utils.surveys.FeedbackExtensions.ID;
+
 
 @ApplicationScoped
 public class FeedbackRepository {
@@ -36,14 +39,29 @@ public class FeedbackRepository {
         return getCollection().find(FeedbackEntity.class).skip(index).limit(count).into(new ArrayList<>());
     }
 
-    @Nullable
-    public FeedbackEntity getFeedbackById(String id) {
-        return getCollection().find(FeedbackEntity.class).filter(new Document("_id", new ObjectId(id))).first();
+    public List<FeedbackEntity> getFeedbacksCreatedAt(LocalDate createdAt, int index, int count) {
+        return getCollection().find(FeedbackEntity.class).filter(new Document(CREATED_AT, createdAt))
+                .skip(index).limit(count).into(new ArrayList<>());
+    }
+
+    public List<FeedbackEntity> getFeedbacksCreatedBefore(LocalDate createdAt, int index, int count) {
+        return getCollection().find(FeedbackEntity.class).filter(new Document(CREATED_AT, new Document("$lt", createdAt)))
+                .skip(index).limit(count).into(new ArrayList<>());
+    }
+
+    public List<FeedbackEntity> getFeedbacksCreatedAfter(LocalDate createdAt, int index, int count) {
+        return getCollection().find(FeedbackEntity.class).filter(new Document(CREATED_AT, new Document("$gt", createdAt)))
+                .skip(index).limit(count).into(new ArrayList<>());
+    }
+
+    public List<FeedbackEntity> getFeedbacksCreatedBetween(LocalDate start, LocalDate end, int index, int count) {
+        return getCollection().find(FeedbackEntity.class).filter(new Document(CREATED_AT, new Document("$gte", start).append("$lte", end))).
+                skip(index).limit(count).into(new ArrayList<>());
     }
 
     @Nullable
-    public FeedbackEntity getFeedbackBySurveyId(String surveyId) {
-        return getCollection().find(FeedbackEntity.class).filter(new Document("surveyId", new ObjectId(surveyId))).first();
+    public FeedbackEntity getFeedbackById(String id) {
+        return getCollection().find(FeedbackEntity.class).filter(new Document(ID, new ObjectId(id))).first();
     }
 
     @Nullable
@@ -52,10 +70,10 @@ public class FeedbackRepository {
     }
 
     public UpdateResult updateFeedback(Feedback feedback) {
-        return getCollection().replaceOne(new Document("_id", new ObjectId(feedback.getId())), FeedbackExtensions.toEntity(feedback));
+        return getCollection().replaceOne(new Document(ID, new ObjectId(feedback.getId())), FeedbackExtensions.toEntity(feedback));
     }
 
     public DeleteResult deleteFeedback(String id) {
-        return getCollection().deleteOne(new Document("_id", new ObjectId(id)));
+        return getCollection().deleteOne(new Document(ID, new ObjectId(id)));
     }
 }
--- src/api/src/main/java/fr/iut/uca/repository/surveys/SurveyRepository.java
+++ src/api/src/main/java/fr/iut/uca/repository/surveys/SurveyRepository.java
@@ -8,19 +8,25 @@ import com.mongodb.lang.Nullable;
 import fr.iut.uca.DatabaseClient;
 import fr.iut.uca.entity.surveys.FeedbackEntity;
 import fr.iut.uca.entity.surveys.SurveyEntity;
-import fr.iut.uca.entity.surveys.SurveyEntity;
-import fr.iut.uca.model.surveys.Survey;
 import fr.iut.uca.model.surveys.Survey;
 import fr.iut.uca.utils.surveys.SurveyExtensions;
 import jakarta.inject.Inject;
 import org.bson.Document;
 import org.bson.types.ObjectId;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SurveyRepository {    @Inject
-DatabaseClient mongoClient;
+import static fr.iut.uca.utils.surveys.FeedbackExtensions.SURVEY_ID;
+import static fr.iut.uca.utils.surveys.SurveyExtensions.ID;
+import static fr.iut.uca.utils.surveys.SurveyExtensions.CREATED_AT;
+import static fr.iut.uca.utils.surveys.SurveyExtensions.PUBLISHED_AT;
+import static fr.iut.uca.utils.surveys.SurveyExtensions.END_AT;
+
+public class SurveyRepository {
+    @Inject
+    DatabaseClient mongoClient;
 
     private MongoCollection<SurveyEntity> getCollection() {
         return mongoClient.getCollection(DatabaseClient.CollectionName.SURVEYS, SurveyEntity.class);
@@ -33,13 +39,73 @@ DatabaseClient mongoClient;
         return getCollection().find(SurveyEntity.class).skip(index).limit(count).into(new ArrayList<>());
     }
 
+    public List<SurveyEntity> getSurveysCreatedAt(LocalDate createdAt, int index, int count) {
+        return getCollection().find(SurveyEntity.class).filter(new Document(CREATED_AT, createdAt))
+                .skip(index).limit(count).into(new ArrayList<>());
+    }
+
+    public List<SurveyEntity> getSurveysCreatedBefore(LocalDate createdAt, int index, int count) {
+        return getCollection().find(SurveyEntity.class).filter(new Document(CREATED_AT, new Document("$lt", createdAt)))
+                .skip(index).limit(count).into(new ArrayList<>());
+    }
+
+    public List<SurveyEntity> getSurveysCreatedAfter(LocalDate createdAt, int index, int count) {
+        return getCollection().find(SurveyEntity.class).filter(new Document(CREATED_AT, new Document("$gt", createdAt)))
+                .skip(index).limit(count).into(new ArrayList<>());
+    }
+
+    public List<SurveyEntity> getSurveysCreatedBetween(LocalDate start, LocalDate end, int index, int count) {
+        return getCollection().find(SurveyEntity.class).filter(new Document(CREATED_AT, new Document("$gt", start).append("$lt", end)))
+                .skip(index).limit(count).into(new ArrayList<>());
+    }
+
+    public List<SurveyEntity> getSurveysPublishedAt(LocalDate publishedAt, int index, int count) {
+        return getCollection().find(SurveyEntity.class).filter(new Document(PUBLISHED_AT, publishedAt))
+                .skip(index).limit(count).into(new ArrayList<>());
+    }
+
+    public List<SurveyEntity> getSurveysPublishedBefore(LocalDate publishedAt, int index, int count) {
+        return getCollection().find(SurveyEntity.class).filter(new Document(PUBLISHED_AT, new Document("$lt", publishedAt)))
+                .skip(index).limit(count).into(new ArrayList<>());
+    }
+
+    public List<SurveyEntity> getSurveysPublishedAfter(LocalDate publishedAt, int index, int count) {
+        return getCollection().find(SurveyEntity.class).filter(new Document(PUBLISHED_AT, new Document("$gt", publishedAt)))
+                .skip(index).limit(count).into(new ArrayList<>());
+    }
+
+    public List<SurveyEntity> getSurveysPublishedBetween(LocalDate start, LocalDate end, int index, int count) {
+        return getCollection().find(SurveyEntity.class).filter(new Document(PUBLISHED_AT, new Document("$gt", start).append("$lt", end)))
+                .skip(index).limit(count).into(new ArrayList<>());
+    }
+
+    public List<SurveyEntity> getSurveysEndAt(LocalDate expiredAt, int index, int count) {
+        return getCollection().find(SurveyEntity.class).filter(new Document(END_AT, expiredAt))
+                .skip(index).limit(count).into(new ArrayList<>());
+    }
+
+    public List<SurveyEntity> getSurveysEndBefore(LocalDate expiredAt, int index, int count) {
+        return getCollection().find(SurveyEntity.class).filter(new Document(END_AT, new Document("$lt", expiredAt)))
+                .skip(index).limit(count).into(new ArrayList<>());
+    }
+
+    public List<SurveyEntity> getSurveysEndAfter(LocalDate expiredAt, int index, int count) {
+        return getCollection().find(SurveyEntity.class).filter(new Document(END_AT, new Document("$gt", expiredAt)))
+                .skip(index).limit(count).into(new ArrayList<>());
+    }
+
+    public List<SurveyEntity> getSurveysEndBetween(LocalDate start, LocalDate end, int index, int count) {
+        return getCollection().find(SurveyEntity.class).filter(new Document(END_AT, new Document("$gt", start).append("$lt", end)))
+                .skip(index).limit(count).into(new ArrayList<>());
+    }
+
     @Nullable
     public SurveyEntity getSurveyById(String id) {
-        return getCollection().find(SurveyEntity.class).filter(new Document("_id", new ObjectId(id))).first();
+        return getCollection().find(SurveyEntity.class).filter(new Document(ID, new ObjectId(id))).first();
     }
 
     public List<FeedbackEntity> getFeedbacksBySurveyId(String surveyId) {
-        return getCollection().find(FeedbackEntity.class).filter(new Document("survey_id", new ObjectId(surveyId))).into(new ArrayList<>());
+        return getCollection().find(FeedbackEntity.class).filter(new Document(SURVEY_ID, new ObjectId(surveyId))).into(new ArrayList<>());
     }
     
     @Nullable
@@ -48,10 +114,10 @@ DatabaseClient mongoClient;
     }
 
     public UpdateResult updateSurvey(Survey survey) {
-        return getCollection().replaceOne(new Document("_id", new ObjectId(survey.getId())), SurveyExtensions.toEntity(survey));
+        return getCollection().replaceOne(new Document(ID, new ObjectId(survey.getId())), SurveyExtensions.toEntity(survey));
     }
 
     public DeleteResult deleteSurvey(String id) {
-        return getCollection().deleteOne(new Document("_id", new ObjectId(id)));
+        return getCollection().deleteOne(new Document(ID, new ObjectId(id)));
     }
 }
