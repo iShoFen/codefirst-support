0b9d1af980be783144657dd16c8c20f0622f7273
Florent MARQUES <florent.marques@etu.uca.fr>
2023-06-09 09:48:13 +0200

    :lipstick: comment item

---
+++ src/app/components/issues/comments/CommentItem.tsx
@@ -0,0 +1,48 @@
+import {StyleSheet, Text, View} from "react-native";
+import {Comment} from "../../../model/issues/Comment";
+import {Divider} from "react-native-paper";
+
+type CommentItemProps = {
+  comment: Comment
+}
+
+export default function CommentItem(props: CommentItemProps) {
+
+  const {
+    comment
+  } = props
+
+  return (<View style={styles.container}>
+    <View style={styles.header}>
+      <Text style={styles.author}>{comment.author}</Text>
+      <Text>-</Text>
+      <Text style={styles.createdAt}>{comment.createdAt.toLocaleDateString()}</Text>
+    </View>
+    <Divider bold/>
+    <Text style={styles.content}>{comment.content}</Text>
+  </View>)
+}
+
+const styles = StyleSheet.create({
+  container: {
+    gap: 4,
+    padding: 8,
+    backgroundColor: 'white',
+    borderBottomWidth: 1,
+    borderColor: 'lightgray'
+  },
+  header: {
+    flexDirection: 'row',
+    gap: 4,
+    alignItems: 'center'
+  },
+  author: {
+    fontWeight: 'bold',
+    fontSize: 18
+  },
+  createdAt: {
+  },
+  content: {
+    fontSize: 16
+  }
+})
