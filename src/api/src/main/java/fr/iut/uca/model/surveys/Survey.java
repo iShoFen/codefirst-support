6e19726a38699988da8592090c3e58ebb59b56ce
Samuel SIRVEN <samuel.sirven@etu.uca.fr>
2023-06-15 00:38:03 +0200

    👔 update mode to handle exception on build

--- src/api/src/main/java/fr/iut/uca/model/surveys/Survey.java
+++ src/api/src/main/java/fr/iut/uca/model/surveys/Survey.java
@@ -25,18 +25,37 @@ public class Survey {
     private Feedback feedback;
 
     public Survey(String id, String title,LocalDate createdAt,LocalDate publishedAt,LocalDate endAt, String description, List<Question> questions, Feedback feedbacks) {
+        if (id == null || id.isBlank()) {
+            throw new IllegalArgumentException("The id cannot be null or blank.");
+        }
+
         this.id = id;
-        this.title = title;
+
+        if (createdAt == null) {
+            throw new IllegalArgumentException("The creation date cannot be null.");
+        }
+
         this.createdAt = createdAt;
-        this.publishedAt = publishedAt;
-        this.endAt = endAt;
-        this.description = description;
-        this.questions.addAll(questions);
-        this.feedback = feedbacks;
+        init(title, publishedAt, endAt, description, questions);
     }
 
-    public Survey(String title,LocalDate createdAt,LocalDate publishedAt,LocalDate endAt, String description, List<Question> questions) {
-        this(null, title, createdAt, publishedAt, endAt, description, questions, null);
+    public Survey(String title, LocalDate createdAt,LocalDate publishedAt,LocalDate endAt, String description, List<Question> questions) throws IllegalArgumentException {
+        this.id = null;
+
+        if (createdAt == null) {
+            throw new IllegalArgumentException("The creation date cannot be null.");
+        }
+
+        this.createdAt = createdAt;
+        init(title, publishedAt, endAt, description, questions);
+    }
+
+    private void init(String title, LocalDate publishedAt, LocalDate endAt, String description, List<Question> questions) throws IllegalArgumentException {
+        setTitle(title);
+        setPublishedAt(publishedAt);
+        setEndAt(endAt);
+        setDescription(description);
+        questions.forEach(this::addQuestion);
     }
 
     public String getId() {
@@ -47,7 +66,11 @@ public class Survey {
         return title;
     }
 
-    public void setTitle(String title) {
+    public void setTitle(String title) throws IllegalArgumentException {
+        if (title == null || title.isBlank()) {
+                throw new IllegalArgumentException("The title cannot be null or blank.");
+            }
+
         this.title = title;
     }
 
@@ -59,7 +82,10 @@ public class Survey {
         return publishedAt;
     }
 
-    public void setPublishedAt(LocalDate publishedAt) {
+    public void setPublishedAt(LocalDate publishedAt) throws IllegalArgumentException {
+        if (publishedAt != null && publishedAt.isBefore(createdAt)) {
+            throw new IllegalArgumentException("The published date cannot be before the creation date.");
+        }
         this.publishedAt = publishedAt;
     }
 
@@ -67,7 +93,10 @@ public class Survey {
         return endAt;
     }
 
-    public void setEndAt(LocalDate endAt) {
+    public void setEndAt(LocalDate endAt) throws IllegalArgumentException {
+        if (endAt != null && endAt.isBefore(publishedAt)) {
+            throw new IllegalArgumentException("The end date cannot be before the published date.");
+        }
         this.endAt = endAt;
     }
 
@@ -75,7 +104,10 @@ public class Survey {
         return description;
     }
 
-    public void setDescription(String description) {
+    public void setDescription(String description) throws IllegalArgumentException {
+        if (description == null || description.isBlank()) {
+            throw new IllegalArgumentException("The description cannot be null or blank.");
+        }
         this.description = description;
     }
 
@@ -83,7 +115,15 @@ public class Survey {
         return Collections.unmodifiableList(questions);
     }
 
-    public void addQuestion(Question question) {
+    public void addQuestion(Question question) throws IllegalArgumentException {
+        if (question == null) {
+            throw new IllegalArgumentException("The question cannot be null.");
+        }
+
+        if (questions.contains(question)) {
+            throw new IllegalArgumentException("The question is already in the survey.");
+        }
+
         questions.add(question);
     }
 
