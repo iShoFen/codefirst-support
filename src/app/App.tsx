514d3c13ed0a490ac716a89c8c229c84c338e02b
Florent MARQUES <florent.marques@etu.uca.fr>
2023-06-18 22:14:49 +0200

    :sparkles: add constants and useFetch

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,6 @@
 import BottomBarNavigation from "./navigation/BottomBarNavigation";
 import store from "./redux/store";
 import {Provider} from "react-redux";
-import {useColors} from "./themes/hooks/useColors";
 
 export default function App() {
   return (
---
+++ src/app/config/constants.ts
@@ -0,0 +1,9 @@
+const BASE_URL = "https://codefirst.iut.uca.fr/containers/florentmarques-codefirst-support-api/api/"
+
+const ISSUES_URL = BASE_URL + "issues"
+const ISSUE_MODELS_URL = BASE_URL + "issues/models"
+const COMMENTS_URL = BASE_URL + "issues/models"
+const SURVEYS_URL = BASE_URL + "surveys"
+const FEEDBACKS_URL = BASE_URL + "feedbacks"
+
+export {ISSUES_URL, ISSUE_MODELS_URL, COMMENTS_URL, SURVEYS_URL, FEEDBACKS_URL}
