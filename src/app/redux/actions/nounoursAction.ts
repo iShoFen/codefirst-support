f7fca007091a345395e75e49e962d3e3c55979d5
Florent MARQUES <florent.marques@etu.uca.fr>
2023-06-24 13:52:34 +0200

    :fire: remove nounours

--- src/app/redux/actions/nounoursAction.ts
+++
@@ -1,8 +0,0 @@
-// import {FETCH_NOUNOURS, SET_THEME} from '../constants';
-//
-// export const setNounoursList = (nounoursList: Nounours[]) => {
-//   return {
-//     type: FETCH_NOUNOURS,
-//     payload: nounoursList,
-//   };
-// }
