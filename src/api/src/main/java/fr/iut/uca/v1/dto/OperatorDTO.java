37af29801e8dd1cc7ec0140fbaa73ed90f735634
Samuel SIRVEN <samuel.sirven@etu.uca.fr>
2023-06-25 03:03:49 +0200

    ✨ update enum dto
    - Update IssueStatusDTO and OperatorDTO to not give UNDEFINED and return null when the given value is lblank or null. Or throw expcetion if is an inccorect value

--- src/api/src/main/java/fr/iut/uca/v1/dto/OperatorDTO.java
+++ src/api/src/main/java/fr/iut/uca/v1/dto/OperatorDTO.java
@@ -1,14 +1,35 @@
 package fr.iut.uca.v1.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonValue;
 
 public enum OperatorDTO {
-    @JsonProperty("undefined")
-    UNDEFINED,
     @JsonProperty("equals")
     EQUALS,
     @JsonProperty("before")
     BEFORE,
     @JsonProperty("after")
-    AFTER,
+    AFTER;
+
+    @JsonValue
+    public String getValue() {
+        return this.name().toLowerCase();
+    }
+
+    @JsonCreator
+    public static OperatorDTO fromValue(String value) throws IllegalArgumentException {
+        if (value == null || value.isBlank()) {
+            return null;
+        }
+        for (OperatorDTO operator : OperatorDTO.values()) {
+            if (operator.name().equalsIgnoreCase(value)) {
+                return operator;
+            }
+        }
+
+        throw new IllegalArgumentException("The value " + value + " is not a valid operator");
+    }
+
+
 }
--- src/api/src/main/java/fr/iut/uca/v1/dto/issues/IssueStatusDTO.java
+++ src/api/src/main/java/fr/iut/uca/v1/dto/issues/IssueStatusDTO.java
@@ -1,12 +1,33 @@
 package fr.iut.uca.v1.dto.issues;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonValue;
 
 public enum IssueStatusDTO {
-    @JsonProperty("undefined")
-    UNDEFINED,
     @JsonProperty("open")
     OPEN,
     @JsonProperty("closed")
-    CLOSED
+    CLOSED;
+
+    @JsonValue
+    public String getValue() {
+        return this.name().toLowerCase();
+    }
+
+    @JsonCreator
+    public static IssueStatusDTO fromValue(String value) throws IllegalArgumentException {
+        if (value == null || value.isBlank()) {
+            return null;
+        }
+
+        for (IssueStatusDTO status : IssueStatusDTO.values()) {
+            if (status.name().equalsIgnoreCase(value)) {
+                return status;
+            }
+        }
+
+        throw new  IllegalArgumentException("The value " + value + " is not a valid status");
+    }
+
 }
